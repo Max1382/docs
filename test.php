fd02cbedae93e99f04b127a8a6dc3fc4faead782
Max 1382 <max1382walker@yahoo.com>
2024-10-16 14:43:55 +0100

    added test

--- test.php
+++ test.php
@@ -1,7 +1,5 @@
 <?php
-// Display a simple message
-echo "Hello, World!";
-
-// Display the current date and time
-echo "<br>Current Date and Time: " . date('Y-m-d H:i:s');
+// Your PHP logic here
+$data = "Hello from PHP!";
+echo json_encode(['message' => $data]);
 ?>
